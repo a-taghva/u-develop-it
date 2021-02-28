53cf0a909fbad2f9a4698fb4f5548c35ffe8afa9
a-taghva <taghvali99@gmail.com>
2021-02-28 10:44:46 -0500

    GET, DELETE and CREATE candidates

--- server.js
+++ server.js
@@ -17,6 +17,41 @@ const db = new sqlite3.Database('./db/election.db', err => {
 	console.log('Connected to the election datatbase!');
 });
 
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+// 	console.log(rows)
+// });
+
+// // GET a single candidate 
+// db.get(`SELECT * FROM candidates WHERE id = 2`, (err, row) => {
+// 	if (err) {
+// 		return console.log(err);
+// 	};
+
+	// console.log(row);
+// });
+
+// // DELETE a candidate
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+//     if (err) {
+//         return console.log(err);
+//     };
+
+    // console.log(result, this, this.changes, this.sql);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+    VALUES(?, ?, ?, ?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+// ES5 functino, not arrow function, to use 'this'
+db.run(sql, params, function(err, result) {
+    if (err) {
+        return console.log(err);
+    };
+
+    console.log(result, this.lastID);
+});
+
 // Default response for any other requests(Not Found) Catch all
 app.use((req, res) => {
 	res.status(404).end();
