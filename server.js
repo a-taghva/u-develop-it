eccb8a7f03b0b3bbded28cad14c9980b8cde527b
a-taghva <taghvali99@gmail.com>
2021-02-28 11:35:22 -0500

    API endpoints for GET and DELETE

--- server.js
+++ server.js
@@ -17,6 +17,56 @@ const db = new sqlite3.Database('./db/election.db', err => {
 	console.log('Connected to the election datatbase!');
 });
 
+// GET all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+
+    // use all() method to retrieve all the rows
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        };
+
+        res.json({
+            message: 'success',
+            data: rows,
+        });
+    });
+});
+
+app.get('/api/candidates/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        };
+
+        res.json({
+            message: 'success',
+            data: rows,
+        });
+    });
+});
+
+app.delete('/api/candidates/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        };
+
+        res.json({
+            message: 'success',
+            changes: this.changes,
+        });
+    });
+});
+
 // db.all(`SELECT * FROM candidates`, (err, rows) => {
 // 	console.log(rows)
 // });
@@ -39,18 +89,18 @@ const db = new sqlite3.Database('./db/election.db', err => {
     // console.log(result, this, this.changes, this.sql);
 // });
 
-// Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-    VALUES(?, ?, ?, ?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-// ES5 functino, not arrow function, to use 'this'
-db.run(sql, params, function(err, result) {
-    if (err) {
-        return console.log(err);
-    };
+// // Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//     VALUES(?, ?, ?, ?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// // ES5 functino, not arrow function, to use 'this'
+// db.run(sql, params, function(err, result) {
+//     if (err) {
+//         return console.log(err);
+//     };
 
-    console.log(result, this.lastID);
-});
+    // console.log(result, this.lastID);
+// });
 
 // Default response for any other requests(Not Found) Catch all
 app.use((req, res) => {
